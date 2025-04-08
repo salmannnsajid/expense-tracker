9fc822342322a61f220e66c56c530d62f6cdff43
Salman Sajid <salman.sajid@argonteq.com>
2025-04-08 19:29:30 +0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ export default function App() {
       }}
     >
       <Paper>
-        {/* Contexts */}
         <AvailableCategoriesContextProvider>
           <HistoryContextProvider>
             <CategoriesContextProvider>
